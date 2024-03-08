d163bf529b6d511758d5764554edf16d99db89b8
Alvaro Guerrero <alvaro.guefer@educa.jcyl.es>
2024-03-08 09:54:50 +0100

    Commit6

--- Admin.java
+++ Admin.java
@@ -2,12 +2,31 @@ import java.util.ArrayList;
 
 public class Admin {
     protected String contraseña;
-    protected ArrayList reservas;
+    protected ArrayList departamentos;
     protected ArrayList salas;
 
     //Constructor
     public Admin (String contraseña){
         this.contraseña = contraseña;
+        this.departamentos = new ArrayList();
+        this.salas = new ArrayList();
     }
+
+    //Añadir departamentos
+    public void añadirDepartamento (Departamento d){
+
+    }
+
+    //Eliminar departamentos
+    public void eliminarDepartamento (Departamento d){
+
+    }
+
+    //Listar departamentos
+    public void listarDepartamentos (){
+
+    }
+
+
 
 }
